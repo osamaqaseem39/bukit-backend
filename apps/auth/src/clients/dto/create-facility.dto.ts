505962e13af444ab3021db0cd4415bb8aa54b722
osamaqaseem39 <osama.qa.39@gmail.com>
2026-03-02 19:04:09 +0500

    latest

--- apps/auth/src/clients/dto/create-facility.dto.ts
+++ apps/auth/src/clients/dto/create-facility.dto.ts
@@ -1,10 +1,25 @@
+import { IsEnum, IsInt, IsObject, IsOptional, IsString, Min } from 'class-validator';
 import { FacilityStatus, FacilityType } from '../facility.entity';
 
 export class CreateFacilityDto {
+  @IsString()
   name: string;
+
+  @IsEnum(FacilityType)
   type: FacilityType;
+
+  @IsOptional()
+  @IsEnum(FacilityStatus)
   status?: FacilityStatus;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
   capacity?: number;
+
+  @IsOptional()
+  @IsObject()
   metadata?: Record<string, any>;
 }
+
 
