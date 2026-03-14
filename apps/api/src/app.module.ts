302b669fbc66f04f232072a88df5240aa8912da3
osamaqaseem39 <osama.qa.39@gmail.com>
2026-03-14 21:48:39 +0500

    latest

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,17 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '../../auth/src/auth.module';
-import { GamingModule } from '../../gaming/src/gaming.module';
-import { SnookerModule } from '../../snooker/src/snooker.module';
-import { TableTennisModule } from '../../table-tennis/src/table-tennis.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    GamingModule,
-    SnookerModule,
-    TableTennisModule,
-  ],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
